f81f63fead6e72f2dd693bfa053354ba4a531cf8
Marco Fantasia <fantasia.marco@gmail.com>
2016-06-15 11:05:50 +0200

    Implementing Kalman Filter on eyes

--- es_face_tracking/Main.cpp
+++ es_face_tracking/Main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 cv::CascadeClassifier face_cascade;
 cv::CascadeClassifier eye_cascade;
 
-cv::Mat displayImage;
+cv::Mat frame;
 std::string main_wnd_name = "Face & Eye Detection";
 
 void detectFace(cv::Mat frame);
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
 	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
-	Mat frame, eye_tpl;
+	Mat eye_tpl;
 	Rect eye_bb;
 	namedWindow(main_wnd_name, 1);
 	while (waitKey(15) != 'q') {
@@ -62,7 +62,7 @@ int main(int argc, char** argv) {
 		flip(frame, frame, 1);
 
 		//Copy frame to image to display
-		frame.copyTo(displayImage);
+		frame.copyTo(frame);
 
 		detectFace(frame);
 		
@@ -73,8 +73,8 @@ int main(int argc, char** argv) {
 		// 1 frm in a msec, quanti frame al secondo?
 		// 1f : a = x : 1
 		// x = 1f*1/a		
-		frame = addFPStoFrame(displayImage, elapsed_time);
-		imshow(main_wnd_name, displayImage);
+		frame = addFPStoFrame(frame, elapsed_time);
+		imshow(main_wnd_name, frame);
 	}
 	cap.release();
 	return 0;
@@ -103,56 +103,26 @@ void detectFace(cv::Mat frame) {
 	// Draw rect on the detected faces
 	for (int i = 0; i < faces.size(); i++) {
 		rectangle(frame, faces[i], Scalar(0, 255, 0), 2);
-		Mat faceROI = gray(faces[i]);
 		//potrei pensare di ridurre faces[i]
 		//invece di dare tutto la faccia, solo la fascia degli occhi
+		//TODO Reduce face region for eye catching
 		//params da paper Roberto Valenti
 		//eye centers are always contained within 2 regions starting from
 		//20%x30% (left eye), and 60%x30% (right eye) of the face region
 		//with dimensions of 25%x20% of the latter
-		
-
+		//Give only half face to cascade
+		Rect halfFace(faces[i].x, faces[i].y, faces[i].width, faces[i].height/2);
+		Mat halfFaceROI = gray(halfFace);
+		//here kalman's magic
 		//eye_cascade
-		eye_cascade.detectMultiScale(faceROI, eyes, 1.1, 5
+		eye_cascade.detectMultiScale(halfFaceROI, eyes, 1.1, 8
 			, 0 | CV_HAAR_SCALE_IMAGE | CV_HAAR_DO_CANNY_PRUNING
-			, cvSize(0, 0), cvSize(100, 100));
+			, cvSize(10, 10), cvSize(100, 100));
 		for (int j = 0; j < eyes.size(); j++) {
 			//effettuo traslazione dei punti per disagnare su frame
 			Rect eye(faces[i].x + eyes[j].x, faces[i].y + eyes[j].y, eyes[j].width, eyes[j].height);
 			rectangle(frame, eye, Scalar(255, 0, 0), 1);
 		}
-
-
-
-
-		/*
-		int x, y, h, w;
-		x = faces[i].x;
-		y = faces[i].y;
-		h = faces[i].height;
-		w = faces[i].width;
-
-		//becco il sinistro
-		int l_x, l_y, l_w, l_h;
-		l_x = x * .15;
-		l_y = y * .2;
-		l_w = w * 0.25;
-		l_h = h * 0.20;
-		int r_x, r_y, r_w, r_h;
-		r_x = x * .50;
-		r_y = y * .2;
-		r_w = w * 0.25;
-		r_h = h * 0.20;
-		int h_eyes = h*.30;
-		int _span_left_e = x*.20;
-		int _span_right_e = x*.60;
-		//creo due img dalla face
-		Rect l_eye(l_x, l_y, l_w, l_h);
-		Rect r_eye(r_x, r_y, r_w, r_h);
-		//Rect myROI(x + _span_left_e, y + h*.30, x + _span_right_e + x*.20, h_eyes);
-		Mat lEye = face(l_eye);
-		Mat rEye = face(r_eye);
-		imshow("l_eye", lEye); imshow("r_eye", rEye);*/
 	}
 }
 
